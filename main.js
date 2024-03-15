7dc26e6a07ccf03f493c3252474b52c88642bd3f
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2024-03-15 11:11:52 -0300

    hj

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
-
+const aba-conteudo = document.querySelectorAll(".aba-conteudo");
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
         for(let j=0;j<botoes.length;j++){
         botoes[j].classList.remove("ativo");
+        aba-conteudo[a].classList.remove("ativo");
     }
     botoes[i].classList.add("ativo")
 }
