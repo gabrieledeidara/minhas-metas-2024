c08689f06839016e9264782b3d0877840f0fbd7f
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2024-04-01 10:31:55 -0300

    wru

--- main.js
+++ main.js
@@ -13,16 +13,16 @@ for(let i=0;i <botoes.length;i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-18T00:00:00");
-let tempoAtual = new Date ();
-//contadores[0].textContent = calculaTempo(tempoObjetivo1); 
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1); 
 
 function calculaTempo(tempoObjetivo1){
-
-let tempoFinal = tempoObjetivo1 - tempoAtual;
-let segundos = Math.floor(tempoFinal/1000);
-let minutos = Math.floor(segundos/60);
-let horas = Math.floor(minutos/60);
-let dias = Math.floor(horas/24);
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
 
 segundos %= 60;
 minutos %= 60;
