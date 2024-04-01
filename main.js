8f10e1ea92a77a7259b5c79a82eb0f0990738397
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2024-04-01 10:33:38 -0300

    r68

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ segundos %= 60;
 minutos %= 60;
 horas %= 24;
 
-contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
 
 
