932c1eef519934bf84772c63a60179a7c97489bc
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2024-04-08 10:17:26 -0300

    dty

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const tempoObjetivo4 = new Date("2026-04-04T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-function atualizaCronometro{
+function atualizaCronometro(){
     for (let i = 0; i < contadores.length; i++){
         contadores[i].textContent = calculaTempo(tempos[i]);
     }
